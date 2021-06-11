656546843ce600ecab0ae485c4a650e72649c46c
Moritz0302 <seifertm@gmx.at>
2021-06-11 12:38:10 +0200

    Next tests were all running till the testDomainsWithDash - Test. Now running as well

--- src/main/java/HttpUrlChecker.java
+++ src/main/java/HttpUrlChecker.java
@@ -29,7 +29,7 @@ public class HttpUrlChecker{
 
 
     public static Matcher getMatcher(String input){
-        Pattern pattern = Pattern.compile("(http://)([a-zA-Z0-9]+)(([.][a-z]+)+)");
+        Pattern pattern = Pattern.compile("(http://)([^.]*(.*)+)(([.][a-z]+)+)");
         Matcher matcher = pattern.matcher(input);
         return matcher;
     }
